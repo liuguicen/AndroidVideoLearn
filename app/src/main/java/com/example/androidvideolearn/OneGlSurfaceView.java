5a96c5abeb16d72b4de0cb40d5310ca9c148ae99
liuguicen <2583657917@qq.com>
2021-11-29 15:35:50 +0800

    画出三角形

--- app/src/main/java/com/example/androidvideolearn/OneGlSurfaceView.java
+++ app/src/main/java/com/example/androidvideolearn/OneGlSurfaceView.java
@@ -28,8 +28,7 @@ public class OneGlSurfaceView extends GLSurfaceView {
     }
 
 
-    private IntBuffer intBufferUtil(int[] arr)
-    {
+    private IntBuffer intBufferUtil(int[] arr) {
         IntBuffer mBuffer;
         // 初始化ByteBuffer，长度为arr数组的长度*4，因为一个int占4个字节
         ByteBuffer qbb = ByteBuffer.allocateDirect(arr.length * 4);
@@ -41,8 +40,7 @@ public class OneGlSurfaceView extends GLSurfaceView {
         return mBuffer;
     }
 
-    private FloatBuffer floatBufferUtil(float[] arr)
-    {
+    private FloatBuffer floatBufferUtil(float[] arr) {
         FloatBuffer mBuffer;
         // 初始化ByteBuffer，长度为arr数组的长度*4，因为一个int占4个字节
         ByteBuffer qbb = ByteBuffer.allocateDirect(arr.length * 4);
@@ -54,7 +52,7 @@ public class OneGlSurfaceView extends GLSurfaceView {
         return mBuffer;
     }
 
-    private ShortBuffer shortBufferUtil(short[] arr){
+    private ShortBuffer shortBufferUtil(short[] arr) {
         ShortBuffer mBuffer;
         // 初始化ByteBuffer，长度为arr数组的长度*2，因为一个short占2个字节
         ByteBuffer dlb = ByteBuffer.allocateDirect(
@@ -73,27 +71,35 @@ public class OneGlSurfaceView extends GLSurfaceView {
 class OneGlRenderer implements GLSurfaceView.Renderer {
 
     private Triangle triangle;
+    private Square square;
+    private Point point;
 
     public void onSurfaceCreated(GL10 unused, EGLConfig config) {
         // Set the background frame color
+        // 这里初始化
         GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        triangle = new Triangle();
+        // triangle = new Triangle();
+        // square = new Square();
+        point = new Point();
     }
+
 
     public void onDrawFrame(GL10 unused) {
         // Redraw background color
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-        triangle.draw();
+        // triangle.draw();
+        // square.draw();
+        point.draw();
     }
 
 
     public void onSurfaceChanged(GL10 unused, int width, int height) {
+        //
         GLES20.glViewport(0, 0, width, height);
     }
 
 
-    private IntBuffer intBufferUtil(int[] arr)
-    {
+    private IntBuffer intBufferUtil(int[] arr) {
         IntBuffer mBuffer;
         // 初始化ByteBuffer，长度为arr数组的长度*4，因为一个int占4个字节
         ByteBuffer qbb = ByteBuffer.allocateDirect(arr.length * 4);
@@ -105,8 +111,7 @@ class OneGlRenderer implements GLSurfaceView.Renderer {
         return mBuffer;
     }
 
-    private FloatBuffer floatBufferUtil(float[] arr)
-    {
+    private FloatBuffer floatBufferUtil(float[] arr) {
         FloatBuffer mBuffer;
         // 初始化ByteBuffer，长度为arr数组的长度*4，因为一个int占4个字节
         ByteBuffer qbb = ByteBuffer.allocateDirect(arr.length * 4);
@@ -118,7 +123,7 @@ class OneGlRenderer implements GLSurfaceView.Renderer {
         return mBuffer;
     }
 
-    private ShortBuffer shortBufferUtil(short[] arr){
+    private ShortBuffer shortBufferUtil(short[] arr) {
         ShortBuffer mBuffer;
         // 初始化ByteBuffer，长度为arr数组的长度*2，因为一个short占2个字节
         ByteBuffer dlb = ByteBuffer.allocateDirect(
@@ -131,7 +136,7 @@ class OneGlRenderer implements GLSurfaceView.Renderer {
         return mBuffer;
     }
 
-    public static int loadShader(int type, String shaderCode){
+    public static int loadShader(int type, String shaderCode) {
 
         // 创造顶点着色器类型(GLES20.GL_VERTEX_SHADER)
         // 或者是片段着色器类型 (GLES20.GL_FRAGMENT_SHADER)
@@ -140,6 +145,97 @@ class OneGlRenderer implements GLSurfaceView.Renderer {
         GLES20.glShaderSource(shader, shaderCode);
         GLES20.glCompileShader(shader);
         return shader;
+    }
+}
+
+class Point {
+    /**
+     * 顶点着色器
+     */
+    private static final String VERTEX_SHADER = "" +
+            // vec4：4个分量的向量：x、y、z、w
+            "attribute vec4 a_Position;\n" +
+            "void main()\n" +
+            "{\n" +
+            // gl_Position：GL中默认定义的输出变量，决定了当前顶点的最终位置
+            "    gl_Position = a_Position;\n" +
+            // gl_PointSize：GL中默认定义的输出变量，决定了当前顶点的大小
+            "    gl_PointSize = 40.0;\n" +
+            "}";
+
+    /**
+     * 片段着色器
+     */
+    private static final String FRAGMENT_SHADER = "" +
+            // 定义所有浮点数据类型的默认精度；有lowp、mediump、highp 三种，但只有部分硬件支持片段着色器使用highp。(顶点着色器默认highp)
+            "precision mediump float;\n" +
+            "uniform mediump vec4 u_Color;\n" +
+            "void main()\n" +
+            "{\n" +
+            // gl_FragColor：GL中默认定义的输出变量，决定了当前片段的最终颜色
+            "    gl_FragColor = u_Color;\n" +
+            "}";
+    private final int mProgram;
+    private final int uColorLocation;
+
+    Point() {
+
+        mProgram = makeProgram(VERTEX_SHADER, FRAGMENT_SHADER);
+// 获取顶点坐标属性在OpenGL程序中的索引
+        int aPositionLocation = GLES20.glGetAttribLocation(mProgram, "a_Position");
+
+// 获取颜色Uniform在OpenGL程序中的索引
+        uColorLocation = GLES20.glGetUniformLocation(mProgram, "u_Color");
+        FloatBuffer mVertexData = GlHelper.createFloatBuffer(new float[]{0, 0, 0});
+// 将缓冲区的指针移动到头部，保证数据是从最开始处读取
+        mVertexData.position(0);
+// 关联顶点坐标属性和缓存数据
+// 1. 位置索引；
+// 2. 每个顶点属性需要关联的分量个数(必须为1、2、3或者4。初始值为4。)；
+// 3. 数据类型；
+// 4. 指定当被访问时，固定点数据值是否应该被归一化(GL_TRUE)或者直接转换为固定点值(GL_FALSE)(只有使用整数数据时)
+// 5. 指定连续顶点属性之间的偏移量。如果为0，那么顶点属性会被理解为：它们是紧密排列在一起的。初始值为0。
+// 6. 数据缓冲区
+        GLES20.glVertexAttribPointer(aPositionLocation, 3, GLES20.GL_FLOAT,
+                false, 0, mVertexData);
+
+// 通知GL程序使用指定的顶点属性索引
+        GLES20.glEnableVertexAttribArray(aPositionLocation);
+    }
+
+    public void draw() {
+        // 步骤1：使用glClearColor设置的颜色，刷新Surface
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+
+        // 更新u_Color的值，即更新画笔颜色
+        GLES20.glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
+
+        // 使用数组绘制图形：1.绘制的图形类型；2.从顶点数组读取的起点；3.从顶点数组读取的数据长度
+        GLES20.glDrawArrays(GLES20.GL_POINTS, 0, 1);
+    }
+
+    /**
+     * 创建OpenGL程序对象
+     *
+     * @param vertexShader   顶点着色器代码
+     * @param fragmentShader 片段着色器代码
+     * @return
+     */
+    protected int makeProgram(String vertexShader, String fragmentShader) {
+        // 步骤1：编译顶点着色器
+        int vertexShaderId = GlHelper.compileVertexShader(vertexShader);
+        // 步骤2：编译片段着色器
+        int fragmentShaderId = GlHelper.compileFragmentShader(fragmentShader);
+        // 步骤3：将顶点着色器、片段着色器进行链接，组装成一个OpenGL程序
+        int mProgram = GlHelper.linkProgram(vertexShaderId, fragmentShaderId);
+
+        if (BuildConfig.DEBUG) {
+            GlHelper.validateProgram(mProgram);
+        }
+
+        // 步骤4：通知OpenGL开始使用该程序
+        GLES20.glUseProgram(mProgram);
+        return mProgram;
     }
 }
 
@@ -160,15 +256,16 @@ class Triangle {
     private FloatBuffer vertexBuffer;
 
     // number of coordinates per vertex in this array
+
     static final int COORDS_PER_VERTEX = 3;
     static float triangleCoords[] = {   // in counterclockwise order:
-            0.0f,  0.5f, 0.0f, // top
+            0.0f, 0.5f, 0.0f, // top 三维的坐标
             -0.5f, -0.5f, 0.0f, // bottom left
             0.5f, -0.5f, 0.0f  // bottom right
     };
 
     // Set color with red, green, blue and alpha (opacity) values
-    float color[] = { 255, 0, 0, 1.0f };
+    float color[] = {255, 0, 0, 1.0f};
     private final int mProgram;
     private int mPositionHandle;
 
@@ -185,19 +282,19 @@ class Triangle {
         vertexBuffer.position(0);
 
         // 创建空的OpenGL ES程序
-        int vertexShader = OneGlRenderer.loadShader(GLES20.GL_VERTEX_SHADER,
-                vertexShaderCode);
-        int fragmentShader = OneGlRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER,
-                fragmentShaderCode);
         mProgram = GLES20.glCreateProgram();
 
-        // 添加顶点着色器到程序中
+        // 加载顶点着色器代码并添加到程序中
+        int vertexShader = OneGlRenderer.loadShader(GLES20.GL_VERTEX_SHADER,
+                vertexShaderCode);
         GLES20.glAttachShader(mProgram, vertexShader);
 
-        // 添加片段着色器到程序中
+        // 加载片段着色器代码并添加到程序中
+        int fragmentShader = OneGlRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER,
+                fragmentShaderCode);
         GLES20.glAttachShader(mProgram, fragmentShader);
 
-        // 创建OpenGL ES程序可执行文件
+        // 链接和创建OpenGL ES程序可执行文件
         GLES20.glLinkProgram(mProgram);
     }
 
@@ -212,10 +309,8 @@ class Triangle {
 
         // 获取顶点着色器的位置的句柄
         mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
-
         // 启用三角形顶点位置的句柄
         GLES20.glEnableVertexAttribArray(mPositionHandle);
-
         //准备三角形坐标数据
         GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX,
                 GLES20.GL_FLOAT, false,
@@ -223,7 +318,6 @@ class Triangle {
 
         // 获取片段着色器的颜色的句柄
         mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
-
         // 设置绘制三角形的颜色
         GLES20.glUniform4fv(mColorHandle, 1, color, 0);
 
@@ -238,19 +332,34 @@ class Triangle {
 }
 
 class Square {
+    private final String vertexShaderCode =
+            "attribute vec4 vPosition;" +
+                    "void main() {" +
+                    "  gl_Position = vPosition;" +
+                    "}";
 
+    private final String fragmentShaderCode =
+            "precision mediump float;" +
+                    "uniform vec4 vColor;" +
+                    "void main() {" +
+                    "  gl_FragColor = vColor;" +
+                    "}";
     private FloatBuffer vertexBuffer;
     private ShortBuffer drawListBuffer;
 
     // number of coordinates per vertex in this array
     static final int COORDS_PER_VERTEX = 3;
     static float squareCoords[] = {
-            -0.5f,  0.5f, 0.0f,   // top left
+            -0.5f, 0.5f, 0.0f,   // top left
             -0.5f, -0.5f, 0.0f,   // bottom left
             0.5f, -0.5f, 0.0f,   // bottom right
-            0.5f,  0.5f, 0.0f }; // top right
+            0.5f, 0.5f, 0.0f}; // top right
 
-    private short drawOrder[] = { 0, 1, 2, 0, 2, 3 }; // order to draw vertices
+    private short drawOrder[] = {0, 1, 2, 0, 2, 3}; // order to draw vertices
+    float color[] = {255, 0, 0, 1.0f};
+    private final int mProgram;
+    private int vertexCount = 4;
+    private final int vertexStride = COORDS_PER_VERTEX * 4; // 4 bytes per vertex
 
     public Square() {
         // 初始化ByteBuffer，长度为arr数组的长度*4，因为一个float占4个字节
@@ -266,5 +375,37 @@ class Square {
         drawListBuffer = dlb.asShortBuffer();
         drawListBuffer.put(drawOrder);
         drawListBuffer.position(0);
+
+        mProgram = GLES20.glCreateProgram();
+        int vertexShader = OneGlRenderer.loadShader(GLES20.GL_VERTEX_SHADER,
+                vertexShaderCode);
+        GLES20.glAttachShader(mProgram, vertexShader);
+        int fragmentShader = OneGlRenderer.loadShader(GLES20.GL_FRAGMENT_SHADER, fragmentShaderCode);
+        GLES20.glAttachShader(mProgram, fragmentShader);
+
+        GLES20.glLinkProgram(mProgram);
+    }
+
+    public void draw() {
+        GLES20.glUseProgram(mProgram);
+        // 获取顶点着色器的位置的句柄
+        int mPositionHandle = GLES20.glGetAttribLocation(mProgram, "vPosition");
+        // 启用三角形顶点位置的句柄
+        GLES20.glEnableVertexAttribArray(mPositionHandle);
+        //准备三角形坐标数据
+        GLES20.glVertexAttribPointer(mPositionHandle, COORDS_PER_VERTEX,
+                GLES20.GL_FLOAT, false,
+                vertexStride, vertexBuffer);
+
+        // 获取片段着色器的颜色的句柄
+        int mColorHandle = GLES20.glGetUniformLocation(mProgram, "vColor");
+        // 设置绘制三角形的颜色
+        GLES20.glUniform4fv(mColorHandle, 1, color, 0);
+
+        // 绘制三角形
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, vertexCount);
+
+        // 禁用顶点数组
+        GLES20.glDisableVertexAttribArray(mPositionHandle);
     }
 }
